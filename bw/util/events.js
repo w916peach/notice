24dd765c25693ec1a64bbbe3573a82bd8f1cfc8c
w916peach <619705604@qq.com>
2018-11-04 12:09:52 +0800

    'a'

--- bw/util/events.js
+++ bw/util/events.js
@@ -1,8 +1,8 @@
 /*
  * @Author: tao 
  * @Date: 2018-10-27 11:43:47 
- * @Last Modified by: tao
- * @Last Modified time: 2018-10-27 11:48:39
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-11-04 12:02:04
  */
 define(['core'],function($){
     $.fn.on = function (type) {
